a3ad0f5f4f842410a44d234173445e16a63ddd4a
hao <unnamed.hao@gmail.com>
2022-07-30 17:42:36 +0800

    add screen size

---
+++ src/config/Def.js
@@ -0,0 +1,6 @@
+const Def = {
+  DesignWidth: 856,
+  ScreenWidth: 856,
+  ScreenHeight: 480,
+};
+export default Def;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import Phaser from 'phaser';
 
+import Def from './config/Def';
+
 class MyGame extends Phaser.Scene {
   constructor() {
     super();
   }
 
   preload() {
+    // 获取屏幕尺寸
+    Def.ScreenWidth = this.cameras.main.width;
+    Def.ScreenHeight = this.cameras.main.height;
+    console.log(`screen size: ${Def.ScreenWidth}x${Def.ScreenHeight}`);
+    // 获取地址栏参数
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const token = urlParams.get('token');
+    console.log(`url localtion: ${queryString} token: ${token}`);
+    // 加载资源
     this.load.image('logo', 'assets/logo.png');
   }
 
   create() {
-    const logo = this.add.image(400, 150, 'logo');
+    const logo = this.add.image(Def.ScreenWidth / 2, 150, 'logo');
 
     this.tweens.add({
       targets: logo,
@@ -26,8 +38,13 @@ class MyGame extends Phaser.Scene {
 const config = {
   type: Phaser.AUTO,
   parent: 'phaser-example',
-  width: 800,
-  height: 600,
+  scale: {
+    width: Def.DesignWidth,
+    height: Math.floor(
+      (Def.DesignWidth / window.innerWidth) * window.innerHeight,
+    ),
+    mode: Phaser.Scale.WIDTH_CONTROLS_HEIGHT,
+  },
   scene: MyGame,
 };
 
